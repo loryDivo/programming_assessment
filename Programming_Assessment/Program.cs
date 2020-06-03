e2e5aa3c364a077101776c3a93a6e36ccaee9e10
Lorenzo DI VITO (contractor) <lorenzo.divito@amadeus.com>
2020-06-03 16:28:33 +0200

    Initial commit

---
+++ Programming_Assessment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programming_Assessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
